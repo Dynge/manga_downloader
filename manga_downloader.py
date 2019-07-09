64bc24b8a9ae808b8ea2e90706d2f0b20386877a
Dynge <michael_lyngs@hotmail.com>
2019-07-09 18:17:14 +0200

    Cleaned code a little

--- manga_downloader.py
+++ manga_downloader.py
@@ -8,22 +8,7 @@ import glob
 import sys
 from subprocess import call
 
-THIS_PATH = os.path.dirname(os.path.realpath(__file__))
-LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
-logging.basicConfig(
-    filename=os.path.join(THIS_PATH, "manga_downloader.log"),
-    level=logging.INFO,
-    format=LOG_FORMAT,
-    filemode='a')
-logger = logging.getLogger()
-LOG_FOLDER = "Logs/"
-SOURCE_LINK = 'https://www.mangareader.net'
-
-regex_chapter_number = re.compile(r".*\D+")
-
 """ Function used in the script """
-
-
 def parsePage(html_string):
     """Parses the raw page to html elements"""
     logger.debug("Parses the raw page to html elements")
@@ -47,12 +32,12 @@ def checkForNewChapter(chapter_list, manga_name):
     Compares the list created by listChapters with the directory and returns the chapters that has not been downloaded.
     """
     manga_documentation_file = LOG_FOLDER + manga_name + ".log"
-    print(manga_documentation_file)
+    #print(manga_documentation_file)
     if os.path.exists(manga_documentation_file):
-        print("File exists")
+        #print("File exists")
         f = open(manga_documentation_file, "r")
         downloaded_chapters = map(lambda chapter: re.sub(r"\n$", "", chapter), f.readlines())
-        print(downloaded_chapters)
+        #print(downloaded_chapters)
         f.close()
         new_chaps = [
             chapter for chapter in chapter_list if chapter[1] not in downloaded_chapters
@@ -133,8 +118,8 @@ def searchForAnime(manga_name):
     """
     Function to search the mangareader site for link to a specific anime.
     """
-    url = "https://www.mangareader.net/alphabetical"
-    page = requests.get(url)
+    manga_list_url = "https://www.mangareader.net/alphabetical"
+    page = requests.get(manga_list_url)
     parsed_page = parsePage(page.text)
     links = parsed_page.find_all('a')
     link_names = [link.string for link in links]
@@ -146,7 +131,7 @@ def searchForAnime(manga_name):
                                             [match[1] for match in matches]))
     return matches
 
-def determineStartEndChapters(chunks, new_chapters):
+def determineStartEndChapters(chunks, new_chapters, chunk_index):
     chunk_start_index = chunks[chunk_index]
     chunk_end_index = chunks[chunk_index + 1] - 1
     chapter_start = re.sub(regex_chapter_number, "", new_chapters[chunk_start_index][1])
@@ -180,6 +165,32 @@ def calculateChapterIndex(amount_of_new_chapters, chunk_amount):
     chunks.append(amount_of_new_chapters)
     return(chunks)
 
+def download_chapters(chapters, chunks):
+    chunk_chapter_indexs = calculateChapterIndex(len(new_chapters), chunks)
+    chunk_index = 0
+    logger.debug("Chunk_chapter_indexs: %s" % chunk_chapter_indexs)
+
+    for i, chapter in enumerate(new_chapters):
+        print("Downloading %s. %s chapters remaining" %
+            (chapter[1], len(new_chapters) - (i + 1)))
+        current_chapter_number = int(re.sub(regex_chapter_number, "", chapter[1]))
+        chapter_start, chapter_end = determineStartEndChapters(chunk_chapter_indexs, new_chapters, chunk_index)
+        if current_chapter_number > int(chapter_end):
+            chunk_index += 1
+            chapter_start, chapter_end = determineStartEndChapters(chunk_chapter_indexs, new_chapters, chunk_index)
+        folder_name = "%s/%s: %s - %s" % (MANGA_TITLE,
+                                        MANGA_TITLE, chapter_start, chapter_end)
+        img_link = download_chapter_page_return_link(
+            chapter[0], chapter[1], folder_name)
+        next_page = nextPageLink(img_link, chapter[1])
+        while type(next_page) == str:
+            img_link = download_chapter_page_return_link(
+                next_page, chapter[1], folder_name)
+            next_page = nextPageLink(img_link, chapter[1])
+        document_downloaded_chapter(MANGA_TITLE, chapter[1])
+        if current_chapter_number == int(chapter_end):
+            convertToKindleAndCleanup(folder_name)
+
 
 def convertToKindleAndCleanup(volume_folder):
     logger.info("Running KindleConversion...")
@@ -189,72 +200,66 @@ def convertToKindleAndCleanup(volume_folder):
     call(["rm", "-rf", volume_folder])
     logger.info("Cleaned.")
 
-""" Inputs search term and finds matches """
-search_term = input("Write the anime to search for: ")
-logger.info("Search term input by user was %s." % search_term)
+LOG_FOLDER = "Logs/"
+SOURCE_LINK = 'https://www.mangareader.net'
 
-search_matches = searchForAnime(search_term)
+if __name__ == "__main__":
+    THIS_PATH = os.path.dirname(os.path.realpath(__file__))
+    LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
+    logging.basicConfig(
+        filename=os.path.join(THIS_PATH, "manga_downloader.log"),
+        level=logging.INFO,
+        format=LOG_FORMAT,
+        filemode='a')
+    logger = logging.getLogger()
 
-if len(search_matches) < 1:
-    print("No matches.")
-    sys.exit()
+    regex_chapter_number = re.compile(r".*\D+")
 
-""" Inputs match index and downloads new chapters """
-match_index = input(
-    "Found the following %s matche(s).\n %s \nPlease select one by indexing it (starting from 0): "
-    % (len(search_matches), [(i, match[1])
-                             for i, match in enumerate(search_matches)]))
-logger.info("Index input by user was %s." % match_index)
+    """ Inputs search term and finds matches """
+    search_term = input("Write the anime to search for: ")
+    logger.info("Search term input by user was %s." % search_term)
 
-if not re.match("\d+", match_index):
-    print("Error! Only numbers allowed!")
-    sys.exit()
+    search_matches = searchForAnime(search_term)
 
-MANGA_TITLE = search_matches[int(match_index)][1]
-match_anime_page = SOURCE_LINK + search_matches[int(match_index)][0]
+    if len(search_matches) < 1:
+        print("No matches.")
+        sys.exit()
 
-match_anime_chapters = listChapters(match_anime_page)
-new_chapters = checkForNewChapter(match_anime_chapters, MANGA_TITLE)
+    """ Inputs match index and downloads new chapters """
+    match_index = input(
+        "Found the following %s matche(s).\n %s \nPlease select one by indexing it (starting from 0): "
+        % (len(search_matches), [(i, match[1])
+                                for i, match in enumerate(search_matches)]))
+    logger.info("Index input by user was %s." % match_index)
 
-if len(new_chapters) < 1:
-    print("No new chapters to download.")
-    sys.exit()
+    if not re.match("\d+", match_index):
+        print("Error! Only numbers allowed!")
+        sys.exit()
 
-chunk_amount = input("The %s new chapter(s) will be seperated into chunks.\n\
-Please select the amount of chuncks.\n\
-Note: -1 will make a chunck for each chapter\n\
-Insert amount of chunks: " % len(new_chapters))
-chunk_amount = int(chunk_amount)
+    MANGA_TITLE = search_matches[int(match_index)][1]
+    match_anime_page = SOURCE_LINK + search_matches[int(match_index)][0]
 
+    match_anime_chapters = listChapters(match_anime_page)
+    new_chapters = checkForNewChapter(match_anime_chapters, MANGA_TITLE)
 
-chunk_chapter_indexs = calculateChapterIndex(len(new_chapters), chunk_amount)
-chunk_index = 0
-logger.debug("Chunk_chapter_indexs: %s" % chunk_chapter_indexs)
-logger.info("Beginning to download chapters...")
-for i, chapter in enumerate(new_chapters):
-    print("Downloading %s. %s chapters remaining" %
-          (chapter[1], len(new_chapters) - (i + 1)))
-    current_chapter_number = int(re.sub(regex_chapter_number, "", chapter[1]))
-    chapter_start, chapter_end = determineStartEndChapters(chunk_chapter_indexs, new_chapters)
-    if current_chapter_number > int(chapter_end):
-        chunk_index += 1
-        chapter_start, chapter_end = determineStartEndChapters(chunk_chapter_indexs, new_chapters)
-    folder_name = "%s/%s: %s - %s" % (MANGA_TITLE,
-                                      MANGA_TITLE, chapter_start, chapter_end)
-    img_link = download_chapter_page_return_link(
-        chapter[0], chapter[1], folder_name)
-    next_page = nextPageLink(img_link, chapter[1])
-    while type(next_page) == str:
-        img_link = download_chapter_page_return_link(
-            next_page, chapter[1], folder_name)
-        next_page = nextPageLink(img_link, chapter[1])
-    document_downloaded_chapter(MANGA_TITLE, chapter[1])
-    if current_chapter_number == int(chapter_end):
-        convertToKindleAndCleanup(folder_name)
+    if len(new_chapters) < 1:
+        print("No new chapters to download.")
+        sys.exit()
+
+    chunk_amount = input("The %s new chapter(s) will be seperated into chunks.\n\
+    Please select the amount of chuncks.\n\
+    Note: -1 will make a chunck for each chapter\n\
+    Insert amount of chunks: " % len(new_chapters))
+    chunk_amount = int(chunk_amount)
 
 
-logger.info("Finished downloading the chapters for the manga: %s." %
-            MANGA_TITLE)
+    logger.info("Beginning to download chapters...")
+    download_chapters(new_chapters, chunk_amount)
+
+
+
+    logger.info("Finished downloading the chapters for the manga: %s." %
+                MANGA_TITLE)
 
 
 
