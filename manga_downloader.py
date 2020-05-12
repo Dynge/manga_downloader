43b3dc067f38f3ef282e24befee10b994a5fb926
Dynge <michael_lyngs@hotmail.com>
2020-05-12 15:55:22 +0200

    Black formatting

--- manga_downloader.py
+++ manga_downloader.py
@@ -9,9 +9,11 @@ import glob
 import sys
 
 """ Function used in the script """
+
+
 def parsePage(html_string):
     """Parses the raw page to html elements"""
-    return BeautifulSoup(html_string, 'html.parser')
+    return BeautifulSoup(html_string, "html.parser")
 
 
 def listChapters(url):
@@ -20,9 +22,11 @@ def listChapters(url):
     """
     html = requests.get(url)
     parsed_html = parsePage(html.text)
-    listing = parsed_html.find(id='listing')
-    chapters = [(SOURCE_LINK + str(link.get('href')), link.string)
-                for link in listing.find_all('a')]
+    listing = parsed_html.find(id="listing")
+    chapters = [
+        (SOURCE_LINK + str(link.get("href")), link.string)
+        for link in listing.find_all("a")
+    ]
     return chapters
 
 
@@ -31,10 +35,12 @@ def checkForNewChapter(chapter_list, manga_name):
     Compares the list created by listChapters with the directory and returns the chapters that has not been downloaded.
     """
     manga_documentation_file = LOG_FOLDER + manga_name + ".log"
-    
+
     if os.path.exists(manga_documentation_file):
         f = open(manga_documentation_file, "r")
-        downloaded_chapters = list(map(lambda chapter: re.sub(r"\n$", "", chapter), f.readlines()))
+        downloaded_chapters = list(
+            map(lambda chapter: re.sub(r"\n$", "", chapter), f.readlines())
+        )
 
         f.close()
         new_chaps = [
@@ -53,23 +59,23 @@ def download_chapter_page_return_link(url, chapter, folder):
     img_html = parsed_pages.img
     saveImage(img_html, folder)
 
-    img_link = img_html.parent.get('href')
-    return (img_link)
+    img_link = img_html.parent.get("href")
+    return img_link
 
 
 def saveImage(img_tag, image_location):
     """Retrieves the img source and extracts the images and saves to file directory"""
 
-    img_url = img_tag['src']
+    img_url = img_tag["src"]
     if not os.path.exists(image_location):
         os.makedirs(image_location)
-    filename = image_location + "/" + img_tag.get('alt') + ".jpg"
-    with open(filename, 'wb+') as f:
-        if 'http' not in img_url:
+    filename = image_location + "/" + img_tag.get("alt") + ".jpg"
+    with open(filename, "wb+") as f:
+        if "http" not in img_url:
             # sometimes an image source can be relative
             # if it is provide the base img_url which also happens
             # to be the site variable atm.
-            img_url = '{}{}'.format(SOURCE_LINK, img_url)
+            img_url = "{}{}".format(SOURCE_LINK, img_url)
         response = requests.get(img_url)
         f.write(response.content)
 
@@ -80,7 +86,7 @@ def document_downloaded_chapter(manga_name, chapter_name):
     """
     if not os.path.exists(LOG_FOLDER):
         os.mkdir(LOG_FOLDER)
-    
+
     manga_documentation_file = LOG_FOLDER + manga_name + ".log"
     f = open(manga_documentation_file, "a")
     f.write(chapter_name + "\n")
@@ -95,9 +101,9 @@ def nextPageLink(img_link, chapter):
     chapter = re.sub(regex_chapter_number, "", chapter)
     if "/%s/" % chapter in img_link:
         next_page = SOURCE_LINK + img_link
-        return (next_page)
+        return next_page
     else:
-        return (False)
+        return False
 
 
 def searchForAnime(manga_name):
@@ -107,14 +113,15 @@ def searchForAnime(manga_name):
     manga_list_url = "https://www.mangareader.net/alphabetical"
     page = requests.get(manga_list_url)
     parsed_page = parsePage(page.text)
-    links = parsed_page.find_all('a')
+    links = parsed_page.find_all("a")
     link_names = [link.string for link in links]
     matches = []
     for i, j in enumerate(link_names):
         if j != None and manga_name.lower() in j.lower():
-            matches.append((links[i].get('href'), j))
-            
+            matches.append((links[i].get("href"), j))
+
     return matches
+
 
 def determineStartEndChapters(chunks, new_chapters, chunk_index):
     chunk_start_index = chunks[chunk_index]
@@ -122,6 +129,7 @@ def determineStartEndChapters(chunks, new_chapters, chunk_index):
     chapter_start = re.sub(regex_chapter_number, "", new_chapters[chunk_start_index][1])
     chapter_end = re.sub(regex_chapter_number, "", new_chapters[chunk_end_index][1])
     return (chapter_start, chapter_end)
+
 
 def calculateChapterIndex(amount_of_new_chapters, chunk_amount):
     """
@@ -132,7 +140,7 @@ def calculateChapterIndex(amount_of_new_chapters, chunk_amount):
     if chunk_amount == -1:
         chunks = [i for i in range(0, amount_of_new_chapters, 1)]
         chunks.append(amount_of_new_chapters)
-        return(chunks)
+        return chunks
     folder_size_floored = math.floor(amount_of_new_chapters / chunk_amount)
     residual_size = amount_of_new_chapters % chunk_amount
     chunks = [x for x in range(0, amount_of_new_chapters, folder_size_floored)]
@@ -148,33 +156,46 @@ def calculateChapterIndex(amount_of_new_chapters, chunk_amount):
         chunk_index -= 1
 
     chunks.append(amount_of_new_chapters)
-    return(chunks)
+    return chunks
+
 
 def download_chapters(manga_title, chapters, chunks):
     """
     Input of chapters and chunks to begin the download of the new chapters and split it into the selected chunk amount.
     """
-    
+
     logger = logging.getLogger()
     chunk_chapter_indexs = calculateChapterIndex(len(chapters), chunks)
     chunk_index = 0
 
     for i, chapter in enumerate(chapters):
-        logger.info("Downloading %s. %s chapters remaining" %
-            (chapter[1], len(chapters) - (i + 1)))
+        logger.info(
+            "Downloading %s. %s chapters remaining"
+            % (chapter[1], len(chapters) - (i + 1))
+        )
         current_chapter_number = int(re.sub(regex_chapter_number, "", chapter[1]))
-        chapter_start, chapter_end = determineStartEndChapters(chunk_chapter_indexs, chapters, chunk_index)
+        chapter_start, chapter_end = determineStartEndChapters(
+            chunk_chapter_indexs, chapters, chunk_index
+        )
         if current_chapter_number > int(chapter_end):
             chunk_index += 1
-            chapter_start, chapter_end = determineStartEndChapters(chunk_chapter_indexs, chapters, chunk_index)
-        folder_name = "%s/%s (%s - %s)" % (manga_title,
-                                        manga_title, chapter_start, chapter_end)
+            chapter_start, chapter_end = determineStartEndChapters(
+                chunk_chapter_indexs, chapters, chunk_index
+            )
+        folder_name = "%s/%s (%s - %s)" % (
+            manga_title,
+            manga_title,
+            chapter_start,
+            chapter_end,
+        )
         img_link = download_chapter_page_return_link(
-            chapter[0], chapter[1], folder_name)
+            chapter[0], chapter[1], folder_name
+        )
         next_page = nextPageLink(img_link, chapter[1])
         while type(next_page) == str:
             img_link = download_chapter_page_return_link(
-                next_page, chapter[1], folder_name)
+                next_page, chapter[1], folder_name
+            )
             next_page = nextPageLink(img_link, chapter[1])
         document_downloaded_chapter(manga_title, chapter[1])
         if current_chapter_number == int(chapter_end):
@@ -185,20 +206,21 @@ def convertToKindleAndCleanup(volume_folder):
     call(["kcc-c2e", volume_folder, "-u", "-m", "-r", "1", "-p", "K578", "-b", "2"])
     call(["rm", "-rf", volume_folder])
 
+
 LOG_FOLDER = "Logs/"
-SOURCE_LINK = 'https://www.mangareader.net'
+SOURCE_LINK = "https://www.mangareader.net"
 regex_chapter_number = re.compile(r".*\D+")
 
 if __name__ == "__main__":
     THIS_PATH = os.path.dirname(os.path.realpath(__file__))
     LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
     logging.basicConfig(
-        #filename=os.path.join(THIS_PATH, "manga_downloader.log"),
+        # filename=os.path.join(THIS_PATH, "manga_downloader.log"),
         level=logging.INFO,
         format=LOG_FORMAT,
-        filemode='a')
+        filemode="a",
+    )
     logger = logging.getLogger()
-
 
     """ Inputs search term and finds matches """
     search_term = input("Write the anime to search for: ")
@@ -213,8 +235,11 @@ if __name__ == "__main__":
     """ Inputs match index and downloads new chapters """
     match_index = input(
         "Found the following %s matche(s).\n %s \nPlease select one by indexing it (starting from 0): "
-        % (len(search_matches), [(i, match[1])
-                                for i, match in enumerate(search_matches)]))
+        % (
+            len(search_matches),
+            [(i, match[1]) for i, match in enumerate(search_matches)],
+        )
+    )
     logger.info('Index input by user was "%s".' % match_index)
 
     if not re.match("\d+", match_index):
@@ -231,21 +256,15 @@ if __name__ == "__main__":
         logger.info("No new chapters to download.")
         sys.exit()
 
-    chunk_amount = input("The %s new chapter(s) will be seperated into chunks.\n\
+    chunk_amount = input(
+        "The %s new chapter(s) will be seperated into chunks.\n\
 Please select the amount of chuncks (max ~50 chapters per chunk is recommended).\n\
 Note: -1 will make a chunck for each chapter\n\
-Insert amount of chunks: " % len(new_chapters))
+Insert amount of chunks: "
+        % len(new_chapters)
+    )
     chunk_amount = int(chunk_amount)
-
 
     logger.info("Beginning to download chapters...")
     download_chapters(MANGA_TITLE, new_chapters, chunk_amount)
-
-
-
-    logger.info("Finished downloading the chapters for the manga: %s." %
-                MANGA_TITLE)
-
-
-
-
+    logger.info("Finished downloading the chapters for the manga: %s." % MANGA_TITLE)
--- manga_updater.py
+++ manga_updater.py
@@ -7,36 +7,49 @@ import argparse
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(
-        description="Script to update your kindle mangas.")
-    parser.add_argument("-a", "--all", action="store_true",
-                        help="Used to automatically update all.")
-    parser.add_argument("-cs", "--chunk_size", default=False,
-                        type=int, nargs=1, help="Used to specify chunk size for all updateable mangas.")
+    parser = argparse.ArgumentParser(description="Script to update your kindle mangas.")
+    parser.add_argument(
+        "-a", "--all", action="store_true", help="Used to automatically update all."
+    )
+    parser.add_argument(
+        "-cs",
+        "--chunk_size",
+        default=False,
+        type=int,
+        nargs=1,
+        help="Used to specify chunk size for all updateable mangas.",
+    )
 
     args = parser.parse_args()
 
     THIS_PATH = os.path.dirname(os.path.realpath(__file__))
     LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
     logging.basicConfig(
-        #filename=os.path.join(THIS_PATH, "manga_updater.log"),
+        # filename=os.path.join(THIS_PATH, "manga_updater.log"),
         level=logging.INFO,
         format=LOG_FORMAT,
-        filemode='a')
+        filemode="a",
+    )
     logger = logging.getLogger()
 
-    mangas_to_update = [re.sub(r".log", "", manga_log)
-                        for manga_log in os.listdir(md.LOG_FOLDER)]
+    mangas_to_update = [
+        re.sub(r".log", "", manga_log) for manga_log in os.listdir(md.LOG_FOLDER)
+    ]
     logger.info("Mangas to update \n%s" % mangas_to_update)
     logger.info("Finding the list of chapters for your mangas...")
     search_results = [md.searchForAnime(manga) for manga in mangas_to_update]
     logger.debug("Search Results \n%s" % search_results)
     exact_matches = [
-        match for manga in search_results for match in manga if match[1] in mangas_to_update]
+        match
+        for manga in search_results
+        for match in manga
+        if match[1] in mangas_to_update
+    ]
     logger.debug("Exact Matches \n%s" % exact_matches)
 
-    chapters_pr_manga = [md.listChapters(
-        md.SOURCE_LINK + manga[0]) for manga in exact_matches]
+    chapters_pr_manga = [
+        md.listChapters(md.SOURCE_LINK + manga[0]) for manga in exact_matches
+    ]
     logger.debug("Listed chapters of size %s" % len(chapters_pr_manga))
     logger.info("Checking your mangas for new chapters...")
 
@@ -44,8 +57,9 @@ if __name__ == "__main__":
     for i, manga in enumerate(exact_matches):
         new_chapters = md.checkForNewChapter(chapters_pr_manga[i], manga[1])
         if len(new_chapters) > 0:
-            logger.info("%s new chapter(s) for manga titled: %s" %
-                  (len(new_chapters), manga[1]))
+            logger.info(
+                "%s new chapter(s) for manga titled: %s" % (len(new_chapters), manga[1])
+            )
             updateable_mangas.append((manga[1], new_chapters))
         else:
             logger.info("No new chapters for manga titled: %s" % manga[1])
@@ -56,21 +70,41 @@ if __name__ == "__main__":
         for manga, new_chapters in updateable_mangas:
             chunks = args.chunk_size
             if not chunks:
-                chunks = int(input("%s new chapter(s) for manga titled: %s\nSelect the amount of chunks to split this into: " % (
-                    len(new_chapters), manga)))
-            logger.info("Downloading %s chapter(s) for manga titled: %s\n" % (
-                len(new_chapters), manga))
+                chunks = int(
+                    input(
+                        "%s new chapter(s) for manga titled: %s\nSelect the amount of chunks to split this into: "
+                        % (len(new_chapters), manga)
+                    )
+                )
+            logger.info(
+                "Downloading %s chapter(s) for manga titled: %s\n"
+                % (len(new_chapters), manga)
+            )
             md.download_chapters(manga, new_chapters, chunks)
     else:
-        manga_index_str = input("Select the manga(s) to update by space-separated index: \n%s\n" % [(i, manga[0]) for i, manga in enumerate(updateable_mangas)])
+        manga_index_str = input(
+            "Select the manga(s) to update by space-separated index: \n%s\n"
+            % [(i, manga[0]) for i, manga in enumerate(updateable_mangas)]
+        )
         manga_index = map(int, manga_index_str.split(" "))
         for index in manga_index:
             chunks = args.chunk_size
             if not chunks:
-                chunks = int(input("%s new chapter(s) for manga titled: %s\nSelect the amount of chunks to split this into: " % (
-                    len(updateable_mangas[index][1]), updateable_mangas[index][0])))
-            logger.info("Downloading %s chapter(s) for manga titled: %s\n" % (
-                len(updateable_mangas[index][1]), updateable_mangas[index][0]))
-            md.download_chapters(updateable_mangas[index][0], updateable_mangas[index][1], chunks)   
-    
+                chunks = int(
+                    input(
+                        "%s new chapter(s) for manga titled: %s\nSelect the amount of chunks to split this into: "
+                        % (
+                            len(updateable_mangas[index][1]),
+                            updateable_mangas[index][0],
+                        )
+                    )
+                )
+            logger.info(
+                "Downloading %s chapter(s) for manga titled: %s\n"
+                % (len(updateable_mangas[index][1]), updateable_mangas[index][0])
+            )
+            md.download_chapters(
+                updateable_mangas[index][0], updateable_mangas[index][1], chunks
+            )
+
     logger.info("Finished updating your mangas.")
